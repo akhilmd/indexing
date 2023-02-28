3fd501a69d9f2a71bb37349dfeab38bffff7597c
akhilmd <akhil.m.durga2@gmail.com>
2023-02-28 22:04:44 +0530

    MB-55751: Add sched token pre check for pause and resume
    Change-Id: I96dc6d41a585edb4038f7158c453a3444aa31ee4

--- secondary/manager/common/token.go
+++ secondary/manager/common/token.go
@@ -1877,7 +1877,34 @@ func CheckInProgressCommandTokensForBucket(bucketUUID string) (_ bool, inProgDef
 
 	// TODO: List and filter build tokens - BuildCommandToken doesn't have BucketUUID
 
-	// TODO: List and filter schedule tokens
+	// Check schedule tokens
+
+	schCreateTokens, err := ListAllScheduleCreateTokens()
+	if err != nil {
+		return false, nil, err
+	}
+
+	stopSchCreateTokens, err := ListAllStopScheduleCreateTokens()
+	if err != nil {
+		return false, nil, err
+	}
+
+	stopSchCreateTokensMap := make(map[common.IndexDefnId]bool)
+	for _, stopSchCreateToken := range stopSchCreateTokens {
+		stopSchCreateTokensMap[stopSchCreateToken.DefnId] = true
+	}
+
+	for _, schCreateToken := range schCreateTokens {
+		// Filter schedule create tokens for bucket
+		if bucketUUID != schCreateToken.BucketUUID {
+			continue
+		}
+
+		// Match with stop schedule create token
+		if _, matchExists := stopSchCreateTokensMap[schCreateToken.Definition.DefnId]; !matchExists {
+			inProgDefns = append(inProgDefns, fmt.Sprintf("DefnId[%v]", schCreateToken.Definition.DefnId))
+		}
+	}
 
 	return len(inProgDefns) > 0, inProgDefns, nil
 }
