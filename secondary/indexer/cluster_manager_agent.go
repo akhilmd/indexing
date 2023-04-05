881f77a7519699cc712e2ca5b67d6a848f687994
akhilmd <akhil.m.durga2@gmail.com>
2023-04-06 03:52:31 +0530

    MB-56344: Add key prefix iterator for local meta
    Change-Id: I709fff0b0a666729ad12f527385c32a351c1e317

--- secondary/indexer/cluster_manager_agent.go
+++ secondary/indexer/cluster_manager_agent.go
@@ -151,6 +151,9 @@ func (c *clustMgrAgent) handleSupvervisorCommands(cmd Message) {
 	case CLUST_MGR_DEL_LOCAL:
 		c.handleDelLocalValue(cmd)
 
+	case CLUST_MGR_GET_LOCAL_WITH_PREFIX:
+		c.handleGetLocalValuesWithPrefix(cmd)
+
 	case CLUST_MGR_DEL_KEYSPACE:
 		c.handleDeleteKeyspace(cmd)
 
@@ -610,6 +613,25 @@ func (c *clustMgrAgent) handleDelLocalValue(cmd Message) {
 	}
 
 }
+
+func (c *clustMgrAgent) handleGetLocalValuesWithPrefix(cmd Message) {
+
+	key := cmd.(*MsgClustMgrLocal).GetKey()
+
+	logging.Infof("ClustMgr:handleGetLocalValuesWithPrefix Key %v", key)
+
+	// Use key as prefix
+	values, err := c.mgr.GetLocalValuesWithKeyPrefix(key)
+
+	c.supvCmdch <- &MsgClustMgrLocal{
+		mType:  CLUST_MGR_GET_LOCAL_WITH_PREFIX,
+		key:    key,
+		values: values,
+		err:    err,
+	}
+
+}
+
 
 func (c *clustMgrAgent) handleDeleteKeyspace(cmd Message) {
 
